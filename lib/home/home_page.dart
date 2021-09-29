1be3e3ef7bc77f298c955c8c844fcf8807a41568
Raul Neto <raul.neto@avenuecode.com>
2021-09-29 09:46:33 -0300

    progredindo nos components

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:DevQuiz/home/widgets/app_bar_widget.dart';
+import 'package:DevQuiz/home/widgets/level_button.dart';
+import 'package:DevQuiz/home/widgets/quiz_card.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,6 +15,40 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 24,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                LevelButton(label: 'Fácil'),
+                LevelButton(label: 'Médio'),
+                LevelButton(label: 'Difícil'),
+                LevelButton(label: 'Perito'),
+              ],
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Expanded(
+              child: GridView.count(
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                crossAxisCount: 2,
+                children: [
+                  QuizCard(),
+                  QuizCard(),
+                  QuizCard(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
